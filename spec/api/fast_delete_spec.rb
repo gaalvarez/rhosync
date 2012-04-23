868dc888f28cf9a03f08a4ab39b7a2751cef0673
Maxim Zverev <max@rhomobile.com>
2012-04-23 11:36:32 -0700

    Implementing fast_insert/update/delete

---
+++ spec/api/fast_delete_spec.rb
@@ -0,0 +1,27 @@
+require File.join(File.dirname(__FILE__),'api_helper')
+
+describe "RhoconnectApiFastDelete" do
+  it_should_behave_like "ApiHelper" do
+    it "should delete an object from rhosync's :md" do
+      data = {'1' => @product1, '2' => @product2, '3' => @product3}
+      @s = Source.load(@s_fields[:name],@s_params)
+      set_state(@s.docname(:md) => data,@s.docname(:md_size) => '3')
+      post "/api/fast_delete", :api_token => @api_token, 
+        :user_id => @u.id, :source_id => @s_fields[:name], :data => {'3' => @product3}
+      last_response.should be_ok
+      data.delete('3')
+      verify_result(@s.docname(:md) => data,@s.docname(:md_size)=>'2')
+    end
+
+    it "should not properly delete the object if fast_delete is called without all the attributes (because fast_delete doesn't ensure any data integrity)" do
+      data = {'1' => @product1, '2' => @product2, '3' => @product3}
+      delete_data = {'3' => {'price' => '1.99'}}
+      @s = Source.load(@s_fields[:name],@s_params)
+      set_state(@s.docname(:md) => data,@s.docname(:md_size) => '3')
+      post "/api/fast_delete", :api_token => @api_token, 
+        :user_id => @u.id, :source_id => @s_fields[:name], :data => delete_data
+      last_response.should be_ok
+      verify_result(@s.docname(:md) => data,@s.docname(:md_size)=>'2')
+    end
+  end  
+end
---
+++ spec/api/fast_update_spec.rb
@@ -0,0 +1,51 @@
+require File.join(File.dirname(__FILE__),'api_helper')
+
+describe "RhoconnectApiFastUpdate" do
+  it_should_behave_like "ApiHelper" do
+    it "should update an attribute and add new one for an object in rhosync's :md" do
+      data = {'1' => @product1, '2' => @product2, '3' => @product3}
+      @s = Source.load(@s_fields[:name],@s_params)
+      set_state(@s.docname(:md) => data,@s.docname(:md_size) => '3')
+      
+      orig_obj_attrs = {'3' => {'price' => @product3['price']}}
+      new_obj_attrs = {'3' => {'price' => '0.99', 'new_attr' => 'new_value'}}
+      
+      post "/api/fast_update", :api_token => @api_token, 
+        :user_id => @u.id, :source_id => @s_fields[:name], :delete_data => orig_obj_attrs, :data => new_obj_attrs
+      last_response.should be_ok
+      data['3'].merge!(new_obj_attrs['3'])
+      verify_result(@s.docname(:md) => data,@s.docname(:md_size)=>'3')
+    end
+    
+    it "should update one attr, add one attr, and remove one attr for an object in rhosync's :md" do
+      data = {'1' => @product1, '2' => @product2, '3' => @product3}
+      @s = Source.load(@s_fields[:name],@s_params)
+      set_state(@s.docname(:md) => data,@s.docname(:md_size) => '3')
+      
+      orig_obj_attrs = {'3' => {'name' => @product3['name'], 'price' => @product3['price']}}
+      new_obj_attrs = {'3' => {'price' => '0.99', 'new_attr' => 'new_value'}}
+      
+      post "/api/fast_update", :api_token => @api_token, 
+        :user_id => @u.id, :source_id => @s_fields[:name], :delete_data => orig_obj_attrs, :data => new_obj_attrs
+      last_response.should be_ok
+      data['3'].delete('name')
+      data['3'].merge!(new_obj_attrs['3'])
+      verify_result(@s.docname(:md) => data,@s.docname(:md_size)=>'3')
+    end
+    
+    it "should remove all attributes , but leave the count incorrect (because fast_update doesn't check if the whole object is deleted)" do
+      data = {'1' => @product1, '2' => @product2, '3' => @product3}
+      @s = Source.load(@s_fields[:name],@s_params)
+      set_state(@s.docname(:md) => data,@s.docname(:md_size) => '3')
+      
+      orig_obj_attrs = {'3' => @product3}
+      new_obj_attrs = {}
+      
+      post "/api/fast_update", :api_token => @api_token, 
+        :user_id => @u.id, :source_id => @s_fields[:name], :delete_data => orig_obj_attrs, :data => new_obj_attrs
+      last_response.should be_ok
+      data.delete('3')
+      verify_result(@s.docname(:md) => data,@s.docname(:md_size)=>'3')
+    end
+  end  
+end
